3d1f2842001f585ef3531b28776d273ff50dd5eb
waikon.tse <waikon.tse@teamrockstars.nl>
2021-02-10 23:06:54 +0100

    Add day-7 problem 2 solution

--- 2020/kotlin/day-7/main.kt
+++ 2020/kotlin/day-7/main.kt
@@ -8,9 +8,13 @@ fun main() {
     val cleanLines = cleanInputLines(fileUtils.lines)
     val mapOfBags = bagsToMap(cleanLines)
 
-    //println (mapOfBags)
+    println (mapOfBags)
 
     println (findTargetBags(mapOfBags, "shiny gold"))
+    println (findTargetBags2(mapOfBags, "shiny gold"))
+    println (findTargetBags2Helper(mapOfBags, "shiny gold"))
+    println (findTargetBags2(mapOfBags, "shiny gold").map { getMultiplier(it)})
+    println (findTargetBags2(mapOfBags, "shiny gold").map { getBag(it)})
 
 }
 
@@ -19,8 +23,7 @@ fun cleanInputLines(rawData: List<String>): List<List<String>> {
         .map { it.replace(" bags contain", ",") }
         .map { it.replace(Regex(" bags?"), "") }
         .map { it.replace(".", "") }
-        //.map { it.replace(Regex(" \\d+ "), "") }
-            .map { it.split(',').map { it.trim() } }
+        .map { it.split(',').map { it.trim() } }
 
 }
 
@@ -49,7 +52,42 @@ fun findTargetBagsHelper(bagsConfig: Map<String, List<String>>, targetBag: Strin
 
 fun findContainerBags(bagsConfig: Map<String, List<String>>, targetBag: String): Set<String> {
     return bagsConfig
-    //.filter { !it.value.map { it.contains(targetBag) }.isEmpty() }
         .filter { it.value.map { it.contains(targetBag) }.contains(true) }
         .keys
+}
+
+
+fun findTargetBags2(bagsConfig: Map<String, List<String>>, targetBag: String): List<String> {
+    return bagsConfig
+        .filter { it.key.contains(targetBag) }
+        .values
+        .flatten()
+}
+
+fun findTargetBags2Helper(bagsConfig: Map<String, List<String>>, targetBag: String): Int {
+    if (bagsConfig.get(targetBag)!!.contains("no other")) {
+        return 1
+    }
+
+    val bagCount = findTargetBags2(bagsConfig, targetBag)
+        .map { getMultiplier(it) * findTargetBags2Helper(bagsConfig, getBag(it)) + getMultiplier(it) * isFinalBag(bagsConfig, getBag(it)) }
+        .sum()
+
+    return bagCount
+}
+
+fun getMultiplier(bag: String): Int {
+    return bag.split(' ')[0].toInt()
+}
+
+fun isFinalBag(bagsConfig: Map<String, List<String>>, targetBag: String): Int {
+    if (bagsConfig.get(targetBag)!!.contains("no other")) {
+        return 0
+    } else {
+        return 1
+    }
+}
+
+fun getBag(bag: String): String {
+    return bag.replace(Regex("\\d+ "), "")
 }
